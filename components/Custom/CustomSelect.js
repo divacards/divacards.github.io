dfb4c6c86be65d63327fe0738720400d4bd0b5fc
Anton Schippmann <cryptoyak@diva.cards>
2021-06-10 11:10:33 +0900

    add prettier

--- components/Custom/CustomSelect.js
+++ components/Custom/CustomSelect.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 import Select from "react-select";
 
 const CustomSelect = (props) => {
-    return (
-        <Select
-          id={props.id}
-          isClearable={props.isClearable}
-          instanceId={props.instanceId}
-          isMulti={props.isMulti}
-          placeholder={props.placeholder}
-          options={props.options}
-          onChange={props.onChange}
-          theme={(theme) => ({
-              ...theme,
-              // borderRadius: 0,
-              colors: {
-                  ...theme.colors,
-                  primary: "hotpink",
-                  neutral10: "lavender",
-              },
-          })}
-          className="p-1 w-12/12 lg:w-3/12"
-        />
-    );
+  return (
+    <Select
+      id={props.id}
+      isClearable={props.isClearable}
+      instanceId={props.instanceId}
+      isMulti={props.isMulti}
+      placeholder={props.placeholder}
+      options={props.options}
+      onChange={props.onChange}
+      theme={(theme) => ({
+        ...theme,
+        // borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          primary: "hotpink",
+          neutral10: "lavender",
+        },
+      })}
+      className="p-1 w-12/12 lg:w-3/12"
+    />
+  );
 };
 
 export default CustomSelect;
