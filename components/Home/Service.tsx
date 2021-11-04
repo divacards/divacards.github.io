4bed02c937ab3efb4ef25b4999bf17680ab3a6d9
Anton Schippmann <cryptoyak@diva.cards>
2021-11-04 12:52:51 +0900

    refactor service & prep translation

--- components/Home/Service.tsx
+++ components/Home/Service.tsx
@@ -5,109 +5,87 @@ import {
   LightningBoltIcon,
   ScaleIcon,
 } from "@heroicons/react/solid";
+import { useTranslation } from "next-export-i18n";
 
-const ServiceHeader = (props) => {
+const ServiceHeader = ({ sectionName, title, desc }) => {
   return (
     <div className="lg:text-center">
-      <p className="uppercase font-semibold text-red-400">
-        {props.sectionName}
-      </p>
+      <p className="uppercase font-semibold text-red-400">{sectionName}</p>
       <h2 className="text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-        {props.title}
+        {title}
       </h2>
       <p className="mt-4 max-w-screen-xl text-xl leading-7 text-lightgray lg:mx-auto">
-        {props.desc}
+        {desc}
       </p>
     </div>
   );
 };
 
-const ServiceItem = (props) => {
+const ServiceItem = ({ icon, title, desc }) => {
   return (
     <div className="flex space-x-4">
-      <div>{props.icon}</div>
+      <div>{icon}</div>
       <div>
-        <h3 className="mb-2 font-semibold text-gray">{props.title}</h3>
-        <p className="leading-normal text-lightgray">{props.desc}</p>
+        <h3 className="mb-2 font-semibold text-gray">{title}</h3>
+        <p className="leading-normal text-lightgray">{desc}</p>
       </div>
     </div>
   );
 };
 
 const Service = () => {
+  const { t } = useTranslation();
+
   const services = [
     {
+      Icon: ScaleIcon,
       title: "Trading",
       desc: "Trade or exchange with anyone either with our partners or in person. Our market partner include OpenSea.io and Adam.jp, both provide wonderful services.",
     },
     {
+      Icon: BeakerIcon,
       title: "Crafting",
       desc: "Can't find the item you wanted anywhere ? Try crafting it with disenchanted card parts. There is a chance to a better item during the process.",
     },
     {
+      Icon: LightningBoltIcon,
       title: "Enchanting",
       desc: "Discover the magical power of enchanting by add more power to one of the items, or disenchant the item that is not needed any longer.",
     },
     {
+      Icon: RefreshIcon,
       title: "Recycling",
       desc: "Unwanted NFTs from other NFT projects are welcomed here. Instead of leaving it in the wallet, smash it into pieces and craft something new.",
     },
   ];
-  const height = 40;
-  const width = 40;
+
   return (
     <section className="flex flex-col items-center lg:justify-around mx-auto mt-10 px-6 lg:px-20 py-8 relative">
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <ServiceHeader
-          title="A Commuity"
-          sectionName="tokyo.cards NFTs"
-          desc="Find the right person in the community to exchange, trade or craft new collectables."
+          title={t("service.header-title")}
+          sectionName={t("service.header-section")}
+          desc={t("service.header-description")}
         ></ServiceHeader>
         <div className="mt-10 relative mx-auto grid lg:grid-cols-2 gap-10 content-center">
-          <ServiceItem
-            icon={
-              <ScaleIcon
-                height={height}
-                width={width}
-                className="text-red-500"
+          {services.map(({ Icon, title, desc }, index) => {
+            const height = 40;
+            const width = 40;
+            return (
+              <ServiceItem
+                key={`service-${index}`}
+                icon={
+                  <Icon
+                    height={height}
+                    width={width}
+                    className="text-red-500"
+                  />
+                }
+                title={title}
+                desc={desc}
               />
-            }
-            title={services[0].title}
-            desc={services[0].desc}
-          ></ServiceItem>
-          <ServiceItem
-            icon={
-              <BeakerIcon
-                height={height}
-                width={width}
-                className="text-red-500"
-              />
-            }
-            title={services[1].title}
-            desc={services[1].desc}
-          ></ServiceItem>
-          <ServiceItem
-            icon={
-              <LightningBoltIcon
-                height={height}
-                width={width}
-                className="text-red-500"
-              />
-            }
-            title={services[2].title}
-            desc={services[2].desc}
-          ></ServiceItem>
-          <ServiceItem
-            icon={
-              <RefreshIcon
-                height={height}
-                width={width}
-                className="text-red-500"
-              />
-            }
-            title={services[3].title}
-            desc={services[3].desc}
-          ></ServiceItem>
+            );
+          })}
         </div>
       </div>
     </section>
