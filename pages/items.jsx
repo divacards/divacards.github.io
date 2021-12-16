408ed1f062508bf0e0669a08df628c51a074d638
void-anderson <anderson@diva.cards>
2021-12-16 10:51:06 +0900

    client side rendering

--- pages/items.jsx
+++ pages/items.jsx
@@ -1,6 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import Image from "next/image";
 import Layout from "../components/Layout";
 
+
+function getQPara(arg) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(arg);
+}
+
 export default function Items() {
+
+    const [res, setContents] = useState([]);
+
+    useEffect(() => {
+        const id = getQPara("id");
+        fetch(`https://diva.cards/api/items/${id}/`)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)
+                setContents(data)
+            })
+    }, []);
 
     return (
         <Layout pageTitle="tokyo.cards">
@@ -9,15 +29,16 @@ export default function Items() {
                     <div className="border-obsidian-gold border-b-2 w-1/2 m-auto" > </div>
                 </div>
                 <span className="w-1/3 mx-auto lufddo text-center lg:text-2xl text-diablo-dark-gold place-self-center">
+                    {/* {res.name} */}
                     static
                 </span>
                 <div className="flex flex-row mx-auto w-1/3 text-cinnabar">
                     <div className="border-obsidian-gold border-b-2 w-1/2 m-auto" > </div>
                 </div>
             </section>
-            {/* <section className="grid grid-cols-3 gap-4">
+            <section className="grid grid-cols-3 gap-4">
                 <div className="text-cinnabar text-center m-5">
-                    <Image
+                    {/* <Image
                         height={500}
                         width={250}
                         loader={({ src }) => src}
@@ -25,16 +46,16 @@ export default function Items() {
                         src={res.image}
                         alt="Main image"
                         className="auto-image"
-                    />
+                    /> */}
                 </div>
                 <div className="text-cinnabar col-span-2 m-5">
-                    {res.name}
+                    {/* {res.name}
                     {res.attributes.map((attr, index) => (
                         <div key={index} >{attr.trait_type}: {attr.value}</div>
-                    ))}
+                    ))} */}
                 </div>
 
-            </section> */}
+            </section>
         </Layout>
     );
 }
